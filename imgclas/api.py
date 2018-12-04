13d6f524631d215365c96206899259b6f690384b
ignacio <ignacio.heredia.cacha@gmail.com>
2018-12-04 20:02:32 +0100

    added get_train_args

--- imgclas/api.py
+++ imgclas/api.py
@@ -24,6 +24,7 @@ import tempfile
 import warnings
 from datetime import datetime
 import pkg_resources
+import builtins
 
 import numpy as np
 import requests
@@ -43,48 +44,53 @@ from imgclas.train_runfile import train_fn
 
 # Set the timestamp
 timestamps = os.listdir(paths.get_models_dir())
-if 'api' in timestamps:
-    TIMESTAMP = 'api'
+
+if not timestamps:
+    warnings.warn("""No available model timestamps for prediction. Therefore the API can only be used for training.""")
+
 else:
-    TIMESTAMP = sorted(timestamps)[-1]
-paths.timestamp = TIMESTAMP
-print('Using TIMESTAMP={}'.format(TIMESTAMP))
+    if 'api' in timestamps:
+        TIMESTAMP = 'api'
+    else:
+        TIMESTAMP = sorted(timestamps)[-1]
+    paths.timestamp = TIMESTAMP
+    print('Using TIMESTAMP={}'.format(TIMESTAMP))
 
-# Set the checkpoint model to use to make the prediction
-ckpts =  os.listdir(paths.get_checkpoints_dir())
-if 'final_model.h5' in ckpts:
-    MODEL_NAME = 'final_model.h5'
-else:
-    MODEL_NAME = sorted([name for name in ckpts if name.endswith('*.h5')])[-1]
-print('Using MODEL_NAME={}'.format(MODEL_NAME))
+    # Set the checkpoint model to use to make the prediction
+    ckpts =  os.listdir(paths.get_checkpoints_dir())
+    if 'final_model.h5' in ckpts:
+        MODEL_NAME = 'final_model.h5'
+    else:
+        MODEL_NAME = sorted([name for name in ckpts if name.endswith('*.h5')])[-1]
+    print('Using MODEL_NAME={}'.format(MODEL_NAME))
 
-TOP_K = 5  # number of top classes predictions to save
+    TOP_K = 5  # number of top classes predictions to save
 
-# Load the class names and info
-splits_dir = paths.get_ts_splits_dir()
-class_names = load_class_names(splits_dir=splits_dir)
-class_info = None
-if 'info.txt' in os.listdir(splits_dir):
-    class_info = load_class_info(splits_dir=splits_dir)
-    if len(class_info) != len(class_names):
-        warnings.warn("""The 'classes.txt' file has a different length than the 'info.txt' file.
-        If a class has no information whatsoever you should leave that classes row empty or put a '-' symbol.
-        The API will run with no info until this is solved.""")
-        class_info = None
-if class_info is None:
-    class_info = ['' for _ in range(len(class_names))]
+    # Load the class names and info
+    splits_dir = paths.get_ts_splits_dir()
+    class_names = load_class_names(splits_dir=splits_dir)
+    class_info = None
+    if 'info.txt' in os.listdir(splits_dir):
+        class_info = load_class_info(splits_dir=splits_dir)
+        if len(class_info) != len(class_names):
+            warnings.warn("""The 'classes.txt' file has a different length than the 'info.txt' file.
+            If a class has no information whatsoever you should leave that classes row empty or put a '-' symbol.
+            The API will run with no info until this is solved.""")
+            class_info = None
+    if class_info is None:
+        class_info = ['' for _ in range(len(class_names))]
 
-# Load training configuration
-conf_path = os.path.join(paths.get_conf_dir(), 'conf.json')
-with open(conf_path) as f:
-    conf = json.load(f)
+    # Load training configuration
+    conf_path = os.path.join(paths.get_conf_dir(), 'conf.json')
+    with open(conf_path) as f:
+        conf = json.load(f)
 
-# Load the model
-model = load_model(os.path.join(paths.get_checkpoints_dir(), MODEL_NAME), custom_objects=utils.get_custom_objects())
-graph = tf.get_default_graph()
+    # Load the model
+    model = load_model(os.path.join(paths.get_checkpoints_dir(), MODEL_NAME), custom_objects=utils.get_custom_objects())
+    graph = tf.get_default_graph()
 
-#Allow only certain file extensions
-allowed_extensions = set(['png', 'jpg', 'jpeg', 'PNG', 'JPG', 'JPEG'])
+    #Allow only certain file extensions
+    allowed_extensions = set(['png', 'jpg', 'jpeg', 'PNG', 'JPG', 'JPEG'])
 ########################################################################################################################
 
 
@@ -95,6 +101,11 @@ def catch_error(f):
         except Exception as e:
             raise e
     return wrap
+
+
+def catch_no_models():
+    if not timestamps:
+        raise BadRequest('You have no models in your `./models` folder to be used for inference.')
 
 
 def catch_url_error(url_list):
@@ -138,6 +149,7 @@ def predict_url(urls, merge=True):
     """
     Function to predict an url
     """
+    catch_no_models()
     catch_url_error(urls)
     with graph.as_default():
         pred_lab, pred_prob = predict(model=model,
@@ -158,6 +170,7 @@ def predict_file(filenames, merge=True):
     """
     Function to predict a local image
     """
+    catch_no_models()
     catch_localfile_error(filenames)
     with graph.as_default():
         pred_lab, pred_prob = predict(model=model,
@@ -178,6 +191,7 @@ def predict_data(images, merge=True):
     """
     Function to predict an image in binary format
     """
+    catch_no_models()
     if not isinstance(images, list):
         images = [images]
 
@@ -293,6 +307,46 @@ def train(user_conf):
     train_fn(TIMESTAMP=timestamp, CONF=CONF)
 
 
+@catch_error
+def get_train_args():
+    """
+    Returns a dict of dicts with the following structure to feed the deepaas API parser:
+    { 'arg1' : {'default': '1',     #value must be a string (use json.dumps to convert Python objects)
+                'help': '',         #can be an empty string
+                'required': False   #bool
+                },
+      'arg2' : {...
+                },
+    ...
+    }
+    """
+    train_args = {}
+    default_conf = config.CONF
+    for group, val in sorted(default_conf.items()):
+        for g_key, g_val in sorted(val.items()):
+            gg_keys = g_val.keys()
+
+            # Load optional keys
+            help = g_val['help'] if ('help' in gg_keys) else ''
+            type = getattr(builtins, g_val['type']) if ('type' in gg_keys) else None
+            choices = g_val['choices'] if ('choices' in gg_keys) else None
+
+            # Additional info in help string
+            help += '\n' + "Group name: {}".format(str(group))
+            if choices: help += '\n' + "Choices: {}".format(str(choices))
+            if type: help += '\n' + "Type: {}".format(g_val['type'])
+
+            opt_args = {'default': json.dumps(g_val['value']),
+                        'help': help,
+                        'required': False}
+            # if type: opt_args['type'] = type # this breaks the submission because the json-dumping
+            #                                     => I'll type-check args inside the train_fn
+
+            train_args[g_key] = opt_args
+    return train_args
+
+
+@catch_error
 def get_metadata():
     """
     Function to read metadata
