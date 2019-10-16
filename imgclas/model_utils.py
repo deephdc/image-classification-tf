4a908e42e4d4999c4d24b94bae2a1c13176e3513
ignacio <ignacio.heredia.cacha@gmail.com>
2019-10-16 19:03:54 +0200

    add pretrained imagenet default

--- imgclas/model_utils.py
+++ imgclas/model_utils.py
@@ -10,6 +10,8 @@ Github: ignacioheredia
 import os
 import json
 
+import numpy as np
+from tensorflow import keras
 from tensorflow.keras import applications
 from tensorflow.keras import regularizers
 from tensorflow.keras import backend as K
@@ -19,7 +21,7 @@ from tensorflow.python.saved_model.signature_def_utils import predict_signature_
 from tensorflow.python.saved_model import tag_constants
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Flatten
 
-from imgclas import paths
+from imgclas import paths, config, utils
 
 
 model_modes = {'DenseNet121': 'torch', 'DenseNet169': 'torch', 'DenseNet201': 'torch',
@@ -39,7 +41,7 @@ def create_model(CONF):
 
     # create the base pre-trained model
     img_width, img_height = CONF['model']['image_size'], CONF['model']['image_size']
-    base_model = architecture(weights='imagenet', include_top=False, input_shape = (img_width, img_height, 3))
+    base_model = architecture(weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
 
     # Add custom layers at the top to adapt it to our problem
     x = base_model.output
@@ -126,3 +128,51 @@ def save_conf(conf):
             txt_file.write("{:<25}{:<30}{:<15} \n".format(key, g_key, str(g_val)))
         txt_file.write('-' * 75 + '\n')
     txt_file.close()
+
+
+def save_default_imagenet_model():
+    """
+    Create a model in models_dir with default ImageNet training
+    """
+    CONF = config.get_conf_dict()
+    TIMESTAMP = 'default_imagenet'
+
+    # Clear default conf and create custom conf
+    for k, v in CONF.items():
+        if k in ['general', 'augmentation']:
+            continue
+        for i, j in v.items():
+            CONF[k][i] = None
+    CONF['augmentation']['train_mode'] = None
+
+    CONF['model']['modelname'] = 'ResNet50'
+    CONF['model']['image_size'] = 224
+    CONF['model']['preprocess_mode'] = model_modes[CONF['model']['modelname']]
+    CONF['model']['num_classes'] = 1000
+    CONF['dataset']['mean_RGB'] = [123.675, 116.28, 103.53]
+    CONF['dataset']['std_RGB'] = [58.395, 57.12, 57.375]
+
+    paths.timestamp = TIMESTAMP
+    paths.CONF = CONF
+
+    # Create classes.txt for ImageNet
+    fpath = keras.utils.get_file(
+        'imagenet_class_index.json',
+        'https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json',
+        cache_subdir='models',
+        file_hash='c2c37ea517e94d9795004a39431a14cb')
+    with open(fpath) as f:
+        classes = json.load(f)
+    classes = np.array(list(classes.values()))[:, 1]
+
+    # Create the model
+    architecture = getattr(applications, CONF['model']['modelname'])
+    img_width, img_height = CONF['model']['image_size'], CONF['model']['image_size']
+    model = architecture(weights='imagenet', include_top=True, input_shape=(img_width, img_height, 3))
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Save everything
+    utils.create_dir_tree()
+    np.savetxt(os.path.join(paths.get_ts_splits_dir(), 'classes.txt'), classes, fmt='%s', delimiter='/n')
+    save_conf(CONF)
+    model.save(os.path.join(paths.get_checkpoints_dir(), 'final_model.h5'))
